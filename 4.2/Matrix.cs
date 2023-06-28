9265c926c8242dfa5fcfa411c08cd6bb15da5d47
Mihael Kardash <mixan193@gmail.com>
2023-06-28 19:58:12 +0300

    Добавьте файлы проекта.

---
+++ 4.2/Matrix.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    internal class Matrix
+    {
+        private int m, n;
+        private int[,] matrix;
+        private static Random random = new Random();
+
+        public Matrix(int m, int n)
+        {
+            this.m = m;
+            this.n = n;
+            matrix = new int[m, n];
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next(999);
+                }
+            }
+        }
+
+        public int[,] GetMatrix()
+        {
+            return matrix;
+        }
+
+        public int GetN() 
+        {
+            return n;
+        }
+        
+        public int GetM() 
+        {
+            return m; 
+        }
+
+        public static Matrix Addition(Matrix matrix1, Matrix matrix2)
+        {
+            if(!(matrix1.m == matrix2.m && matrix1.n == matrix2.n))
+            {
+                Console.WriteLine("У матриц разный размер");
+                return null;
+            }
+            Matrix result = new Matrix(matrix1.m, matrix1.n);
+            for(int i = 0; i < matrix1.m; i++)
+            {
+                for(int j = 0; j < matrix1.n; j++)
+                {
+                    result.matrix[i, j] = matrix1.matrix[i, j] + matrix2.matrix[i, j];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 4.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m1 = new Matrix(3, 4);
+            Matrix m2 = new Matrix(3, 4);
+            Matrix m3 = Matrix.Addition(m1, m2);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(m1.GetMatrix()[i, j].ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(m2.GetMatrix()[i, j].ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(m3.GetMatrix()[i, j].ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
