8d4d65b75661d23617901b00347483e92583cd2a
Mihael Kardash <mixan193@gmail.com>
2023-06-28 20:03:22 +0300

    Мелкие изменения

--- 4.2/Program.cs
+++ 4.2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _4._2
 {
@@ -13,33 +9,24 @@ namespace _4._2
             Matrix m1 = new Matrix(3, 4);
             Matrix m2 = new Matrix(3, 4);
             Matrix m3 = Matrix.Addition(m1, m2);
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(m1.GetMatrix()[i, j].ToString().PadLeft(5));
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(m2.GetMatrix()[i, j].ToString().PadLeft(5));
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(m3.GetMatrix()[i, j].ToString().PadLeft(5));
-                }
-                Console.WriteLine();
-            }
+            WriteMatrix(m1);
+            WriteMatrix(m2);
+            WriteMatrix(m3);
             Console.ReadKey();
+
+        }
+
+        private static void WriteMatrix(Matrix m)
+        {
+            for (int i = 0; i < m.GetM(); i++)
+            {
+                for (int j = 0; j < m.GetN(); j++)
+                {
+                    Console.Write(m.GetMatrix()[i, j].ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 
         }
     }
